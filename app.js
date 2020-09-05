4257dde044518a94b50a66a874f14b398752c4e2
Luu92 <luu.vale.05@gmail.com>
2020-09-04 20:34:40 -0500

    se arreglo agregar producto

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ class Product {
 class UI {
     
     addProduct(product){
-        const productList = document.getElementById("product-list")
+    
+        const productList = document.querySelector('.product-list')
+        //const productList = document.getElementById("product-list")
         const element = document.createElement('div')   
         element.innerHTML = `
             <div class="card text-center mb-4" >
                 <div class="card-body">
                     <strong>Producto</strong>: ${product.name}
-                    <strong>Precio</strong>: ${product.prince}
+                    <strong>Precio</strong>: ${product.price}
                     <strong>Año</strong>: ${product.year}
                 </div>
             </div>
@@ -24,7 +26,7 @@ class UI {
     }
 
     deleteProduct(){
-
+        
     }
 
     showMessage(){
@@ -35,6 +37,8 @@ class UI {
 // DOM
 
 document.getElementById('product-form').addEventListener('submit', (event) =>{
+    event.preventDefault()
+    
     const name = document.getElementById('name').value
     const price = document.getElementById('price').value
     const year = document.getElementById('year').value
@@ -44,5 +48,6 @@ document.getElementById('product-form').addEventListener('submit', (event) =>{
     const ui = new UI()
     ui.addProduct(product)
 
-    event.preventDefault()
 })
+const ui = new UI()
+ui.addProduct(new Product("dasd",144,2021))
