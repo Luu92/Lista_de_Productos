4bbd2c28e51d02dce6399c5b0a0885dc180f3705
Luu92 <luu.vale.05@gmail.com>
2020-09-05 19:00:50 -0500

    App terminada y probada con responsive

--- app.js
+++ app.js
@@ -19,18 +19,36 @@ class UI {
                     <strong>Producto</strong>: ${product.name}
                     <strong>Precio</strong>: ${product.price}
                     <strong>Año</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete">Eliminar</a>
                 </div>
             </div>
         `;
         productList.appendChild(element)
     }
 
-    deleteProduct(){
-        
+    resetFrom(){
+        document.getElementById('product-form').reset();
     }
 
-    showMessage(){
 
+    deleteProduct(element){
+        if(element.name === 'delete' ){
+            element.parentElement.parentElement.parentElement.remove()
+            this.showMessage('Producto Eliminado','danger')
+        }
+    }
+
+    showMessage(message, cssClass){
+       const div = document.createElement('div')
+       div.className = `alert alert-${cssClass} mt-1 text-center`
+       div.appendChild(document.createTextNode(message))
+       //Mostrando en el DOM
+       const container = document.querySelector('.container-fluid')
+       const app =  document.querySelector('.app')
+       container.insertBefore(div,app)
+       setTimeout(() => {
+            document.querySelector('.alert').remove()
+       },3000)
     }
 }
 
@@ -46,8 +64,19 @@ document.getElementById('product-form').addEventListener('submit', (event) =>{
     const product = new Product(name,price,year)
 
     const ui = new UI()
+    if(name === '' || price === '' || year === ''){
+        return ui.showMessage('Campos Vacios !!!' , 'warning')
+    }
+
     ui.addProduct(product)
+    ui.resetFrom()
+    ui.showMessage('Producto Agregado al Carrito','primary')
 
 })
 const ui = new UI()
 ui.addProduct(new Product("dasd",144,2021))
+
+document.querySelector('.product-list').addEventListener('click',(element)=>{
+    const ui = new UI()
+    ui.deleteProduct(element.target)
+})
